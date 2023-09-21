4895bfb9c6357b9cbefc6886ce4217fabc7753f6
NureniJamiu <nurenijamiu951@gmail.com>
2023-09-21 07:59:13 +0100

    feat:implement toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
+
+import menu from "../../public/Vector.png";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   const links = [
     { title: "Timeline", url: "#" },
     { title: "Overview", url: "#" },
     { title: "FAQs", url: "#" },
     { title: "Contact", url: "#" },
   ];
+
+  const handleToggle = () => setToggle(!toggle);
   return (
     <nav
       className="px-10 md:px-20 lg:px-28 py-5 md:py-3 border-b border-zinc-700 mx-auto"
@@ -29,22 +36,10 @@ const Navbar = () => {
             ))}
           </div>
 
-          {/* remember to change icon  */}
           <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="md:hidden w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
+            <div className="md:hidden cursor-pointer" onClick={handleToggle}>
+              <Image src={menu} alt="menu icon" width={20} height={20} />
+            </div>
 
             <Button className="hidden md:flex btn-gradient rounded px-10">
               Register
@@ -52,6 +47,61 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
+      {/* mobile navItems fold */}
+      {toggle && (
+        <div
+          className="md:hidden fixed w-full h-screen top-0 left-0 bg-[#150E28] px-10 py-3 z-50 "
+          data-aos="fade-right"
+          data-aos-easing="ease-in-out"
+          // data-aos-duration="1000"
+        >
+          {/* close menu icon */}
+          <div
+            className="float-right bg-gradient-to-b from-purple-500 to-pink-500 rounded-full p-[2px] cursor-pointer"
+            onClick={handleToggle}
+          >
+            <div className="bg-[#150E28] rounded-full p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
+          </div>
+          {/* end close menu icon */}
+          <div className="mt-16 flex flex-col gap-8">
+            {links.map((_, index) => (
+              <Link
+                key={index}
+                href={_.url}
+                className=""
+                data-aos="fade-right"
+                data-aos-duration={`1${index + 1}00`}
+              >
+                {_.title}
+              </Link>
+            ))}
+
+            <Button
+              className="w-32 btn-gradient rounded"
+              data-aos="fade-right"
+              data-aos-duration="1600"
+            >
+              Register
+            </Button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
