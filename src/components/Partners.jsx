7f90821f52d6d918c72dc711534bc8a1cd0b279a
NureniJamiu <nurenijamiu951@gmail.com>
2023-09-21 09:53:11 +0100

    style: change animation style

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -6,14 +6,22 @@ import sponsors from "../../public/partners.png";
 const Partners = () => {
   return (
     <section className=" px-10 md:px-20 lg:px-28 py-12">
-      <div className="md:w-[480px] text-center md:h-auto mx-auto">
+      <div
+        className="md:w-[480px] text-center md:h-auto mx-auto"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
         <h2 className="font-clash text-3xl">Partners and Sponsors</h2>
-        <p className="my-5" data-aos="fade-left" data-aos-duration="1500">
+        <p className="my-5">
           Getlinked Hackathon 1.0 is honored to have the following major
           companies as its partners and sponsors
         </p>
       </div>
-      <div className="relative h-44 md:h-64 lg:h-96 mt-12">
+      <div
+        className="relative h-44 md:h-64 lg:h-96 mt-12"
+        data-aos="fade-up"
+        data-aos-duration="1500"
+      >
         <Image src={sponsors} alt="sponsors" fill />
       </div>
     </section>
